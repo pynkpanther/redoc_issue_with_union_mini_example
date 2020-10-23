afc08544b0308d5ac00a2526f3c38cd2a4c373ac
Klaus Strohmenger <klaus.strohmenger@charite.de>
2020-10-23 11:49:04 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # redoc_issue_with_union_mini_example
-redoc_issue_with_union_mini_example
+
+example to demenstrate redoc bug.
+
+run:
+
+```bash
+uvicorn --host=0.0.0.0 --port=8000 redoc_issue_with_union_mini_example.main:app --reload
+```
+
+go to http://localhost:8888/redoc and see example payload for circle.
+
+redoc seems to generate examples for each class in Union and adds parameters of the example from main.example_shape on top.
---
+++ redoc_issue_with_union_mini_example/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Body
+from typing import Union
+from pydantic import BaseModel, Field
+
+
+class Circle(BaseModel):
+    circle_center_x: int = Field(example=100)
+    circle_center_y: int = Field(example=200)
+    circle_radius: int = Field(example=500)
+
+
+class Rectangle(BaseModel):
+    rect_upper_left_x: int = Field(example=10)
+    rect_upper_left_y: int = Field(example=20)
+    rect_width: int = Field(example=30)
+    rect_height: int = Field(example=40)
+
+
+UnionInput = Union[
+    Circle,
+    Rectangle,
+]
+
+example_shape = Body(
+    None,
+    description="Nice description",
+    example=Rectangle(
+        rect_upper_left_x=42,
+        rect_upper_left_y=42,
+        rect_width=42,
+        rect_height=42,
+    ),
+)
+
+
+app = FastAPI()
+
+
+@app.post("/shapes")
+async def post_shapes(
+    shape: UnionInput = example_shape,
+):
+    return 200
